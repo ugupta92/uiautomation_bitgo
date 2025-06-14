694863776d1df73cc2a6102ea70b56103551932a
ugupta92 <umang.nitk@gmail.com>
2025-06-14 14:45:13 +0530

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,6 @@
+import unittest
+from transaction_test import TestSuite
 
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSuite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
